354562f2d5cda18f212d7a1a8c3e7f621943d7ef
allibrdoine <allibrodine81@gmail.com>
2022-09-19 17:29:25 -0500

    comment model created

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
+//add prefix of `/comments`
+router.use('/comments', commentRoutes);
 //add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
+
 
 module.exports = router;
